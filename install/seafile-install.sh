113f9a0df0542b25d1ba1bbba85bcfbf9c6da277
root <root@smithers.lan>
2026-04-10 12:37:52 +0200

    Run Seafile auto setup via python3

--- install/seafile-install.sh
+++ install/seafile-install.sh
@@ -174,7 +174,7 @@ export SEAFILE_ADMIN_PASSWORD='${SEAFILE_ADMIN_PASSWORD}'
 EOF_SETUP
 chown ${SEAFILE_USER}:${SEAFILE_USER} ${SEAFILE_SETUP_ENV}
 chmod 600 ${SEAFILE_SETUP_ENV}
-runuser -u ${SEAFILE_USER} -- bash -lc "source ${SEAFILE_SETUP_ENV}; source ${SEAFILE_ROOT}/python-venv/bin/activate; cd ${SEAFILE_INSTALL_DIR}; ./setup-seafile-mysql.py auto -n \"\${SEAFILE_SERVER_NAME}\" -i \"\${SEAFILE_SERVER_HOSTNAME}\" -p \"\${SEAFILE_FILESERVER_PORT}\" -e 1 -o 127.0.0.1 -t 3306 -u seafile -w \"\${SEAFILE_DB_PASS}\" -c ccnet_db -s seafile_db -b seahub_db"
+runuser -u ${SEAFILE_USER} -- bash -lc "source ${SEAFILE_SETUP_ENV}; source ${SEAFILE_ROOT}/python-venv/bin/activate; cd ${SEAFILE_INSTALL_DIR}; python3 ./setup-seafile-mysql.py auto -n \"\${SEAFILE_SERVER_NAME}\" -i \"\${SEAFILE_SERVER_HOSTNAME}\" -p \"\${SEAFILE_FILESERVER_PORT}\" -e 1 -o 127.0.0.1 -t 3306 -u seafile -w \"\${SEAFILE_DB_PASS}\" -c ccnet_db -s seafile_db -b seahub_db"
 if [[ ! -L "${SEAFILE_ROOT}/seafile-server-latest" ]]; then
   ln -sfn "${SEAFILE_INSTALL_DIR}" "${SEAFILE_ROOT}/seafile-server-latest"
 fi
